fc441ce2d6ca7aefe9ed74bf3d1322a1064560c8
vaal12-go <vaal12@gmx.com>
2024-02-27 20:40:25 +0200

    Triple display texture is working

--- js/scene1.js
+++ js/scene1.js
@@ -6,6 +6,12 @@ class Example extends Phaser.Scene {
   frm1;
   img1;
   i = 0;
+  tileArr;
+  main_tile_plate;
+  screenTilesArrays;
+
+  SCENE_ROW_LEN = 20
+  SCENE_ROW_NO = 27
 
   preload() {
     this.load.image("bg1", "img/red.png");
@@ -13,7 +19,89 @@ class Example extends Phaser.Scene {
     this.load.image("tls", "img/tiles_packed_32.png");
   }
 
+  fillArray(tileID) {
+    var scrArr = []
+    for(var y=0; y<this.SCENE_ROW_NO;y++) {
+      scrArr.push([])
+      for (var x=0;x<this.SCENE_ROW_LEN;x++) {
+        //this.main_tile_plate.draw(this.tileArr[tileID], x*32, y*32)
+        scrArr[y].push(tileID)
+      }
+    }
+    console.log('scrArr.length :>> ', scrArr.length);
+    console.log('scrArr :>> ', scrArr);
+    return scrArr
+  }
+
+  prepareTileArr() {
+    this.tls_txture = this.textures.get("tls")
+    // this.tls_txture.getSourceImage().width
+    var row_len = this.tls_txture.getSourceImage().width / 32
+    console.log(' row_len :>> ',  row_len);
+    var no_rows = this.tls_txture.getSourceImage().height / 32
+    console.log('no_rows :>> ', no_rows);
+
+
+    //Populate tiles array
+    this.tileArr = []
+
+    var i=0
+    for (var y=0; y<no_rows;y++) {
+      // console.log('New line :>> ');
+      for(var x=0;x<row_len;x++) {
+        // console.log('i :>> ', i);
+        var x_coord = x*32
+        // console.log('x_coord :>> ', x_coord);
+
+        var y_coord = y*32
+        // console.log('y_coord :>> ', y_coord);
+        this.tileArr.push(
+          this.tls_txture.add("frm_"+i, 0, x*32, y*32, 32, 32)
+        )
+        i++
+      }
+      // if (y>2) break;
+    }
+  }
+
   create() {
+
+    this.main_tile_plate = this.add
+    .renderTexture(0, 0, this.game.config.width, this.game.config.height*3)
+    .setOrigin(0);
+
+    console.log('this.game.config.height*3 :>> ', this.game.config.height*3);
+
+    this.prepareTileArr()
+
+    this.screenTilesArrays = [] //This will hold actual tiles to be drawn on the screen
+
+    var scr1 = this.fillArray(50)
+    this.screenTilesArrays.push(scr1)
+
+    var scr2 = this.fillArray(56)
+    this.screenTilesArrays.push(scr2)
+
+    var scr3 = this.fillArray(42)
+    this.screenTilesArrays.push(scr3)
+
+    console.log('this.SCENE_ROW_NO :>> ', this.SCENE_ROW_NO);
+    console.log('this.game.config.height :>> ', this.game.config.height);
+    console.log('this.SCENE_ROW_LEN :>> ', this.SCENE_ROW_LEN);
+
+    for(var scr=0; scr<this.screenTilesArrays.length; scr++) {
+      console.log('scr*this.game.config.height :>> ', scr*this.game.config.height);
+      for(var y=0; y<this.SCENE_ROW_NO;y++) {
+        for (var x=0;x<this.SCENE_ROW_LEN;x++) {
+          this.main_tile_plate.draw(
+                this.tileArr[this.screenTilesArrays[scr][y][x]], x*32, 
+                    (y)*32+scr*this.game.config.height)
+          // console.log('(y)*32+scr*this.game.config.height :>> ', (y)*32+scr*this.game.config.height);
+          // console.log('this.screenTilesArrays[scr][x][y] :>> ', this.screenTilesArrays[scr][x][y]);
+        } 
+      }
+    }
+    this.cameras.main.scrollY = this.game.config.height*2;
 
     // Tileset example: https://phaser.io/examples/v3/view/tilemap/base-tile-size
     // this.tls_txture  = this.add
@@ -22,63 +110,44 @@ class Example extends Phaser.Scene {
 
     // this.tls_txture.draw("tls", 0, 0)
 
-    this.tls_txture = this.textures.get("tls")
-    // this.tls_txture.getSourceImage().width
-    var row_len = this.tls_txture.getSourceImage().width / 32
-    console.log(' row_len :>> ',  row_len);
-    var no_rows = this.tls_txture.getSourceImage().height / 32
-    console.log('no_rows :>> ', no_rows);
+    
 
-    var tileArr = []
+    // fill this.main_tile_plate with grass - ID:50
+    
+    
+    
+    // this.cameras.main.scrollY = this.game.config.height*2;
 
-    var i=0
-    for (var y=0; y<no_rows;y++) {
-      console.log('New line :>> ');
-      for(var x=0;x<row_len;x++) {
-        console.log('i :>> ', i);
-        var x_coord = x*32
-        console.log('x_coord :>> ', x_coord);
-
-        var y_coord = y*32
-        console.log('y_coord :>> ', y_coord);
-        tileArr.push(
-          this.tls_txture.add("frm_"+i, 0, x*32, y*32, 32, 32)
-        )
-        i++
-      }
-      // if (y>2) break;
-    }
-
-    console.log('this.tls_txture.getSourceImage().width :>> ', this.tls_txture.getSourceImage().width);
+    // console.log('this.tls_txture.getSourceImage().width :>> ', this.tls_txture.getSourceImage().width);
 
     // Frame: https://photonstorm.github.io/phaser3-docs/Phaser.Textures.Frame.html
     // this.frm1 = this.tls_txture.add("frm1", 0, 31, 31, 32, 32)
 
 
-    console.log("this.game.config.width :>> ", this.game.config.width);
-    this.add.image(0, 0, "ybg").setOrigin(0);
-    this.img1 = this.add.image(0, 0, "bg1").setOrigin(0);
-    this.rt = this.add
-      .renderTexture(0, 0, this.game.config.width, this.game.config.height)
-      .setOrigin(0);
+    // console.log("this.game.config.width :>> ", this.game.config.width);
+    // this.add.image(0, 0, "ybg").setOrigin(0);
+    // this.img1 = this.add.image(0, 0, "bg1").setOrigin(0);
+    // this.rt = this.add
+    //   .renderTexture(0, 0, this.game.config.width, this.game.config.height)
+    //   .setOrigin(0);
 
-    const cam = this.cameras.main;
-    // this.cameras.main.setOrigin(0)
-    this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2);
+    // const cam = this.cameras.main;
+    // // this.cameras.main.setOrigin(0)
+    // this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2);
 
-    this.rt.beginDraw();
-    var k = 0;
-    for (k = 0; k < 800; k = k + 40) {
-      this.rt.draw("bg1", k, this.i + k);
-      for(var z=72; z<tileArr.length;z++) {
-        this.rt.draw(tileArr[z], (z-72)*40, this.i+400)
-      }
-      // this.rt.draw(this.frm1, k, this.i + k+10)
-    }
-    this.i += 10;
-    if (this.i > 200) this.i = 0;
+    // this.rt.beginDraw();
+    // var k = 0;
+    // for (k = 0; k < 800; k = k + 40) {
+    //   this.rt.draw("bg1", k, this.i + k);
+    //   for(var z=72; z<tileArr.length;z++) {
+    //     this.rt.draw(tileArr[z], (z-72)*40, this.i+400)
+    //   }
+    //   // this.rt.draw(this.frm1, k, this.i + k+10)
+    // }
+    // this.i += 10;
+    // if (this.i > 200) this.i = 0;
 
-    this.rt.endDraw();
+    // this.rt.endDraw();
     // console.log('cam :>> ', cam);
 
     // const src = this.textures.get('bg1').getSourceImage();
@@ -89,13 +158,18 @@ class Example extends Phaser.Scene {
   }
 
   update() {
+
+    this.cameras.main.scrollY -= 2
+    if (this.cameras.main.scrollY <= 0) {
+      this.cameras.main.scrollY = this.game.config.height*2;
+    }
     // console.log('this.game.camera :>> ', this.game.camera);
     // this.rt.camera.rotation -= 0.01;
     // this.rt.clear();
 
     // this.cameras.main.scrollY += 1;
     // console.log('this.cameras.main.scrollY :>> ', this.cameras.main.scrollY);
-    this.img1.y += 2;
+    // this.img1.y += 2;
 
     // this.game.debug.cameraInfo(game.camera, 32, 32);
   }
