63622b6db89ba98d3cd444a16e255fdc1899ccd4
qwe1 <qwe1@qwe.com>
2025-01-30 01:26:04 +0200

    2nd screen is not blitter (via 2nd texture) to the first screen.

--- js/bgUpdater.js
+++ js/bgUpdater.js
@@ -1,146 +1,162 @@
-
-
 class BGUpdater {
-    backgroundDynamicTexture = null;
-    bgTileSetName = "";
-    bgTileSetTexture = null;
-    bgTilesArray = null;
+  backgroundDynamicTexture = null;
+  bgTileSetName = "";
+  bgTileSetTexture = null;
+  bgTilesArray = null;
 
-    shadowScreenArray = [];//This will hold tile numbers (2 screens)
+  shadowScreenArray = []; //This will hold tile numbers (2 screens)
 
-    scene = null;
-    currentCameraPosition = -1;
-    currentShadowArrayRowDrawn = -1;
+  scene = null;
+  currentCameraPosition = -1;
+  currentShadowArrayRowDrawn = -1;
 
-    lastGeneratedShadowRow = null;
-    
+  lastGeneratedShadowRow = null;
 
-    constructor(scene, backgroundTilesetName) {
-        this.scene = scene
-        this.currentCameraPosition = this.scene.game.config.height;
-        this.currentShadowArrayRowDrawn = 0;
+  tempTexture = null;
 
-        this.backgroundDynamicTexture = this.scene.textures.addDynamicTexture(
-            "BGUpdater_backgroundDynamicTexture",
-            this.scene.game.config.width,
-            this.scene.game.config.height * 2
-          );
+  constructor(scene, backgroundTilesetName) {
+    this.scene = scene;
+    this.currentCameraPosition = this.scene.game.config.height;
+    this.currentShadowArrayRowDrawn = 0;
 
-        this.bgTileSetName = backgroundTilesetName;
-        this.bgTileSetTexture = this.scene.textures.get(this.bgTileSetName);
-        this.bgTilesArray = getTileArrayFromTileset(this.bgTileSetTexture, TILE_WIDTH_HEIGHT);
+    this.backgroundDynamicTexture = this.scene.textures.addDynamicTexture(
+      "BGUpdater_backgroundDynamicTexture",
+      this.scene.game.config.width,
+      this.scene.game.config.height * 2
+    );
+    //.setIsSpriteTexture(false)
 
-        const startingMap = this.scene.make.tilemap({ key: "startingMap", tileWidth: 32, tileHeight: 32 });
+    this.tempTexture = this.scene.textures
+      .addDynamicTexture(
+        "BGUpdater_backgroundDynamicTexture_temp",
+        this.scene.game.config.width,
+        this.scene.game.config.height
+      )
+      .setIsSpriteTexture(false);
 
-        // for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {//Filling 1st screen
-        //     var newRowArr2 = [];
-        //     for (var colNo=0; colNo<SCENE_TILES_ROW_LEN; colNo++) {
-        //         newRowArr2.push(42);
-        //     } 
-        //     this.shadowScreenArray.push(newRowArr2);
-        // }//for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {
+    this.bgTileSetName = backgroundTilesetName;
+    this.bgTileSetTexture = this.scene.textures.get(this.bgTileSetName);
+    this.bgTilesArray = getTileArrayFromTileset(
+      this.bgTileSetTexture,
+      TILE_WIDTH_HEIGHT
+    );
 
-        // console.log('startingMap :>> ', startingMap);
-        // console.log('startingMap2.layer.data[0][0] :>> ', startingMap.layer.data[0][0].index);
-        //Filling the shadow array from start tilemap
-        // TODO: expand to two screens
-        // console.log('SCENE_ROW_NO :>> ', SCENE_ROW_NO);
-        for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {//Filling 1st screen
-            var newRowArr = [];
-            for (var colNo=0; colNo<SCENE_TILES_ROW_LEN; colNo++) {
-                // console.log('rowNo :>> ', rowNo);
-                // console.log('colNo :>> ', colNo);
-                // console.log('object :>> ', startingMap.layer.data[rowNo][colNo].index);
-                newRowArr.push(
-                    startingMap.layer.data[rowNo][colNo].index
-                );
-            } 
-            this.shadowScreenArray.push(newRowArr);
-        }//for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {
-        this.lastGeneratedShadowRow = this.shadowScreenArray[0];
+    const startingMap = this.scene.make.tilemap({
+      key: "startingMap",
+      tileWidth: 32,
+      tileHeight: 32,
+    });
 
-        //Drawing shadow array on texture
-        this.drawShadowArray();
-        
+    // for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {//Filling 1st screen
+    //     var newRowArr2 = [];
+    //     for (var colNo=0; colNo<SCENE_TILES_ROW_LEN; colNo++) {
+    //         newRowArr2.push(42);
+    //     }
+    //     this.shadowScreenArray.push(newRowArr2);
+    // }//for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {
 
-        console.log('this.shadowScreenArray :>> ', this.shadowScreenArray);
-        this.generateNextRiverSections();
-        console.log('this.shadowScreenArray AFTER :>> ', this.shadowScreenArray);
-        this.drawShadowArray();
-        console.log('this.shadowScreenArray AFTER2 :>> ', this.shadowScreenArray);
-        this.generateNextRiverSections();
-        this.drawShadowArray();
+    // console.log('startingMap :>> ', startingMap);
+    // console.log('startingMap2.layer.data[0][0] :>> ', startingMap.layer.data[0][0].index);
+    //Filling the shadow array from start tilemap
+    // TODO: expand to two screens
+    // console.log('SCENE_ROW_NO :>> ', SCENE_ROW_NO);
+    for (var rowNo = 0; rowNo < SCENE_ROW_NO; rowNo++) {
+      //Filling 1st screen
+      var newRowArr = [];
+      for (var colNo = 0; colNo < SCENE_TILES_ROW_LEN; colNo++) {
+        // console.log('rowNo :>> ', rowNo);
+        // console.log('colNo :>> ', colNo);
+        // console.log('object :>> ', startingMap.layer.data[rowNo][colNo].index);
+        newRowArr.push(startingMap.layer.data[rowNo][colNo].index);
+      }
+      this.shadowScreenArray.push(newRowArr);
+    } //for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {
+    this.lastGeneratedShadowRow = this.shadowScreenArray[0];
 
-        scene.add.image(
-            0, 0,
-            this.backgroundDynamicTexture
-        ).setOrigin(0);
-    }//constructor(scene, backgroundTilesetName) {
+    //Drawing shadow array on texture
+    this.drawShadowArray();
 
-    drawShadowArray() {
-        var shadowArrRows = this.shadowScreenArray.length;
-        console.log('shadowArrRows :>> ', shadowArrRows);
-        for(var rowNo=shadowArrRows-1; rowNo>=0; rowNo--) {
-            for (var colNo=0; colNo<SCENE_TILES_ROW_LEN; colNo++) {
-                this.backgroundDynamicTexture.draw(
-                    this.bgTilesArray[this.shadowScreenArray[rowNo][colNo]],
-                    colNo*TILE_WIDTH_HEIGHT,
-                    this.scene.game.config.height*2 - (this.currentShadowArrayRowDrawn)*TILE_WIDTH_HEIGHT
-                )
-            } 
-            this.shadowScreenArray.splice(this.shadowScreenArray.length-1, 1);
-            this.currentShadowArrayRowDrawn++;
-        }//for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {
-    }//drawShadowArray() {
+    console.log("this.shadowScreenArray :>> ", this.shadowScreenArray);
+    this.generateNextRiverSections();
+    console.log("this.shadowScreenArray AFTER :>> ", this.shadowScreenArray);
+    this.drawShadowArray();
+    console.log("this.shadowScreenArray AFTER2 :>> ", this.shadowScreenArray);
+    this.generateNextRiverSections();
+    this.drawShadowArray();
 
-    update(cameraPosition) {
-        // console.log('cameraPosition :>> ', cameraPosition);
-        if (cameraPosition==0) {
-            console.log('Have restart of camera :>> ');
-            this.currentShadowArrayRowDrawn = SCENE_ROW_NO;
-        }
-        if (cameraPosition % TILE_WIDTH_HEIGHT == 0) {
-            console.log('cameraPosition reaches tiles boundary :>> ', cameraPosition);
-            this.generateNextRiverSections();
-            this.drawShadowArray();
-        }
-    }//update(cameraPosition) {
+    scene.add.image(0, 0, this.backgroundDynamicTexture).setOrigin(0);
+  } //constructor(scene, backgroundTilesetName) {
 
-    generateNextRiverSections() {
-        // console.log('generateNextRiverSections :>> ');
-        // console.log('this.shadowScreenArray :>> ', this.shadowScreenArray);
-        var leftRiverBank = -1;
-        // var lastShadowRowArr = this.shadowScreenArray[this.shadowScreenArray.length-1];
-        // console.log('this.lastGeneratedShadowRow :>> ', this.lastGeneratedShadowRow);
+  drawShadowArray() {
+    var shadowArrRows = this.shadowScreenArray.length;
+    console.log("shadowArrRows :>> ", shadowArrRows);
+    for (var rowNo = shadowArrRows - 1; rowNo >= 0; rowNo--) {
+      for (var colNo = 0; colNo < SCENE_TILES_ROW_LEN; colNo++) {
+        this.backgroundDynamicTexture.draw(
+          this.bgTilesArray[this.shadowScreenArray[rowNo][colNo]],
+          colNo * TILE_WIDTH_HEIGHT,
+          this.scene.game.config.height * 2 -
+            this.currentShadowArrayRowDrawn * TILE_WIDTH_HEIGHT
+        );
+      }
+      this.shadowScreenArray.splice(this.shadowScreenArray.length - 1, 1);
+      this.currentShadowArrayRowDrawn++;
+    } //for(var rowNo=0; rowNo<SCENE_ROW_NO; rowNo++) {
+  } //drawShadowArray() {
 
-        for(var i=0; i<SCENE_TILES_ROW_LEN; i++) {
-            if(this.lastGeneratedShadowRow[i]==51) {
-                leftRiverBank = i;
-                break;
-            }
-        }
-        // console.log('leftRiverBank :>> ', leftRiverBank);
-        var leftBankDecision = Math.floor(Math.random()*4);
-        // console.log('leftBankDecision :>> ', leftBankDecision);
-        var newRow = []
-        //leave the bank as is case
-        newRow = this.lastGeneratedShadowRow.slice(0, 
-            this.lastGeneratedShadowRow.length);
+  update(cameraPosition) {
+    // console.log('cameraPosition :>> ', cameraPosition);
+    if (cameraPosition == 0) {
+      console.log("Have restart of camera :>> ");
+      this.currentShadowArrayRowDrawn = SCENE_ROW_NO;
 
-        switch(leftBankDecision) {
-            case 0: //Widen left bank
+      this.tempTexture.draw("BGUpdater_backgroundDynamicTexture", 0, 0);
+      this.backgroundDynamicTexture.draw(
+        "BGUpdater_backgroundDynamicTexture_temp",
+        0,
+        this.scene.game.config.height
+      );
+    }
+    if (cameraPosition % TILE_WIDTH_HEIGHT == 0) {
+      console.log("cameraPosition reaches tiles boundary :>> ", cameraPosition);
+      this.generateNextRiverSections();
+      this.drawShadowArray();
+    }
+  } //update(cameraPosition) {
 
-                break;
-            case 1: //Leave the bank as is
+  generateNextRiverSections() {
+    // console.log('generateNextRiverSections :>> ');
+    // console.log('this.shadowScreenArray :>> ', this.shadowScreenArray);
+    var leftRiverBank = -1;
+    // var lastShadowRowArr = this.shadowScreenArray[this.shadowScreenArray.length-1];
+    // console.log('this.lastGeneratedShadowRow :>> ', this.lastGeneratedShadowRow);
 
-                break;
-            case 2: //Narrow the left bank
-                break;
-        };//switch(leftBankDecision) {
+    for (var i = 0; i < SCENE_TILES_ROW_LEN; i++) {
+      if (this.lastGeneratedShadowRow[i] == 51) {
+        leftRiverBank = i;
+        break;
+      }
+    }
+    // console.log('leftRiverBank :>> ', leftRiverBank);
+    var leftBankDecision = Math.floor(Math.random() * 4);
+    // console.log('leftBankDecision :>> ', leftBankDecision);
+    var newRow = [];
+    //leave the bank as is case
+    newRow = this.lastGeneratedShadowRow.slice(
+      0,
+      this.lastGeneratedShadowRow.length
+    );
 
-        this.shadowScreenArray.push(newRow);
-        this.lastGeneratedShadowRow = newRow;
+    switch (leftBankDecision) {
+      case 0: //Widen left bank
+        break;
+      case 1: //Leave the bank as is
+        break;
+      case 2: //Narrow the left bank
+        break;
+    } //switch(leftBankDecision) {
 
-    }//generateNextRiverSections() {
-
-}//class BGUpdater {
+    this.shadowScreenArray.push(newRow);
+    this.lastGeneratedShadowRow = newRow;
+  } //generateNextRiverSections() {
+} //class BGUpdater {
